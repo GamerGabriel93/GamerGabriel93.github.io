f40740c87de2b92181709deddedcdfe6e235df61
Gabriel <gamergabyka@gmail.com>
2021-04-23 22:14:11 +0200

    1

--- js/first.js
+++ js/first.js
@@ -4,7 +4,7 @@ function printout() {
         let answear = "A(z) " + szam + " páros!";
         let data = document.getElementById("answear");
         data.innerHTML = answear;
-    }
+    }    
     else {
         let answear = "A(z) " + szam + " páratlan!";
         let data = document.getElementById("answear");
@@ -12,21 +12,6 @@ function printout() {
     }
     /*Ha ezt is megnézed BALÁZS akkor írj messengeren hogy ASUS!!!!!*/
 }
-
-var namebase = ["Gábor", "Balázs"];
-var pwbase = ["1234", "abcd"];
-
-
-function check() {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    if (namebase.includes(username)) {
-        if (pwbase.includes(password)) {
-            alert('Sikeres bejelentkezés!');
-        }
-
-    }
-    else {
-        alert('Helytelen adat!');
-    }
+function login(){
+    window.location.replace("https://gamergabriel93.github.io/parosszam.html")
 }
