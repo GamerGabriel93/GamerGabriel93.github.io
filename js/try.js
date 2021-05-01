830a774750fe1596da77ba127d7ea98aa48d2b79
GamerGabriel93 <gamergabyka@gmail.com>
2021-05-02 00:32:48 +0200

    Update try.js

--- js/try.js
+++ js/try.js
@@ -6,7 +6,7 @@ function first(){
   var value = a.getElementById("valasz");
   var today = new Date();
   document.getElementById("data").value = '';
-  var day = today.getFullYear() + "/" + String(today.getMonth()).padStart(2,'0') + "/" + String(today.getDay()).padStart(2,'0');
+  var day = today.getFullYear() + "/" + String(today.getMonth()+1).padStart(2,'0') + "/" + String(today.getDate()).padStart(2,'0');
   var time = String(today.getHours()).padStart(2,'0') + ":" + String(today.getMinutes()).padStart(2,'0');
   var answear = "Üdvözöljük újra " + adat + "!";
   var answear2 = "<br>" + answear + "<br><br>Utoljára bejelentkezve: " + day + " " + time;
