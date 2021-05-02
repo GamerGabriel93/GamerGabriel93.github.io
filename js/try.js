e61e4049c34b725a680688e0f705375f9b3d62c9
GamerGabriel93 <gamergabyka@gmail.com>
2021-05-02 14:01:56 +0200

    1

--- js/try.js
+++ js/try.js
@@ -1,14 +1,17 @@
 var a = document;
 var table = [];
 
-function first(){
+
+function getname() {
   var adat = a.getElementById("data").value;
-  var value = a.getElementById("valasz");
-  var today = new Date();
+  table.push(adat);
   document.getElementById("data").value = '';
+  window.location.replace("https://gamergabriel93.github.io/teszt2.html");
+
+  /*var today = new Date();
   var day = today.getFullYear() + "/" + String(today.getMonth()+1).padStart(2,'0') + "/" + String(today.getDate()).padStart(2,'0');
   var time = String(today.getHours()).padStart(2,'0') + ":" + String(today.getMinutes()).padStart(2,'0');
   var answear = "Üdvözöljük újra " + adat + "!";
   var answear2 = "<br>" + answear + "<br><br>Utoljára bejelentkezve: " + day + " " + time;
-  value.innerHTML = answear2;
+  value.innerHTML = answear2;*/
 }
