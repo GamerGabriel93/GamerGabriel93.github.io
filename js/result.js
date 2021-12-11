e893ce4cc1c6828491cd27a41d86bffb1215cc5e
GamerGabriel93 <gamergabyka@gmail.com>
2021-12-11 12:23:33 +0100

    test

--- js/result.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', () => {
-    const name = localStorage.getItem('name');
-    const welcome = "Üdvözlünk újra itt " + name + "!";
-
-    document.getElementById("user").innerHTML = welcome;
-})
